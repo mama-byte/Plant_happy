e2b2612a6ffad7727123a3b4c21541e1a0a68765
Ruthie Haskell <ruthie.haskell@gmail.com>
2020-01-09 20:49:13 +0100

    plants controller and views

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -23,10 +23,13 @@ class PlantsController < ApplicationController
   end
 
   def edit
+    @plant = Plant.find(params[:id])
   end
 
   def update
+    @plant = Plant.find(params[:id])
     if @plant.update(plant_params)
+      @plant.save
       redirect_to @plant, notice: 'Plant was updated'
     else
       render :edit
@@ -34,6 +37,7 @@ class PlantsController < ApplicationController
   end
 
   def destroy
+    @plant = Plant.find(params[:id])
     @plant.destroy
     redirect_to plants_url,
     notice: 'Plant was destroyed'
@@ -47,7 +51,7 @@ class PlantsController < ApplicationController
 
   def plant_params
     params.require(:plant).permit(:name, :description, :price,
-                                  :care_instruction, :user_id,
+                                  :care_instructions, :user_id,
                                   :latitude, :longitude)
   end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,10 +1,11 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
   validates :care_instructions, presence: true
   validates :price, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  # validates :latitude, presence: true
+  # validates :longitude, presence: true
 end
