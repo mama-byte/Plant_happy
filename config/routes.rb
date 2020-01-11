5a8049b7cd1adcbdbc3b594b6bc3335b82bb7584
Lesley Burdock <burdocklesley@gmail.com>
2020-01-11 16:23:23 +0100

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'plants#index'
 
   resources :plants do
     resources :bookings, only: [:create, :show, :new]
