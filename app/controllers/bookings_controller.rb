05eeee8ff1bca32651b43880448013a9855fc42c
Lesley Burdock <burdocklesley@gmail.com>
2020-01-11 14:25:32 +0100

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,36 +4,38 @@ class BookingsController < ApplicationController
 
   # To create a booking I need this to be linked to a plant but to show the bookings I need to be linked to a current_user
 
-  def show
-    @booking = Booking.find(params[:id])
-    @plant = Plant.find(params[:plant_id])
-    @booking.plant_id = @plant.id
-  end
-
   def index
     @bookings = current_user.bookings
   end
 
- # def new
- #    @booking = Booking.new
- #    # authorize @plant
- #  end
+  def show
+    @booking = Booking.find(params[:id])
+    # @plant = Plant.find(params[:plant_id])
+    # @booking.plant_id = @plant.id
+  end
 
- #  def create
- #    # you have to set the booking to the plant here !
- #    @booking = Booking.new(booking_params)
- #    @booking.user_id = current_user.id
- #    if @booking.save
- #      redirect_to root_path
- #    else
- #      render 'new'
- #    end
- #  end
+ def new
+    @booking = Booking.new
+    @booking.plant_id = params[:plant_id]
+    # authorize @plant
+  end
+
+  def create
+    # you have to set the booking to the plant here !
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    @booking.plant_id = params[:plant_id]
+    if @booking.save
+      redirect_to user_path
+    else
+      render 'new'
+    end
+  end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to root_path
+    redirect_to user_path
   end
 
   private
