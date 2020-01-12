3c55e18a9603b3472d73fd1dcaea18b2a860d6dd
Sergejs Dementjevs <dozornik88@gmail.com>
2020-01-12 14:29:04 +0100

    Created a working geocode

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,13 +1,15 @@
 class Plant < ApplicationRecord
-
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   mount_uploader :photo, PhotoUploader
   validates :name, presence: true
+  validates :address, presence: true
   validates :description, presence: true
   validates :care_instructions, presence: true
   validates :price, presence: true
-  # validates :latitude, presence: true
-  # validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
---
+++ db/migrate/20200112131750_add_column_to_plants.rb
@@ -0,0 +1,5 @@
+class AddColumnToPlants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :plants, :address, :string
+  end
+end
