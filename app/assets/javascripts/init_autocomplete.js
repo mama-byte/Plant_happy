ff9bab9a25ea49fc8af38e2dc3d596da52a85f8c
Sergejs Dementjevs <dozornik88@gmail.com>
2020-01-14 19:27:40 +0100

    working autocorrect

---
+++ app/assets/javascripts/init_autocomplete.js
@@ -0,0 +1,6 @@
+
+  var placesAutocomplete = places({
+    appId: "plZCVNX96EA2",
+    apiKey: "49158a2ebe0ed769260ceade534dd2b2",
+    container: document.querySelector('#plant_address')
+  });
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,9 +9,7 @@ class PlantsController < ApplicationController
     @markers = @plants.map do |plant|
       {
         lat: plant.latitude,
-        lng: plant.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { plant: plant }),
-        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+        lng: plant.longitude
       }
     end
   end
@@ -24,7 +22,9 @@ class PlantsController < ApplicationController
     @markers = @plants.map do |plant|
       {
         lat: plant.latitude,
-        lng: plant.longitude
+        lng: plant.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { plant: plant }),
+        image_url: helpers.asset_url('cactus.png')
       }
     end
   end
